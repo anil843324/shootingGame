55b88dcd546e9c8668d7668cdbe729cbc715964e
anil843324 <anilkrsingh14200@gmail.com>
2022-05-28 17:12:22 +0530

    working code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,129 +1,117 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from "react";
+import { v4 } from "uuid";
 
-import "./Home.css"
+import "./Home.css";
+
+const initialData = [
+  { id: v4(), color: "red", display: "block", screenCount: 1 },
+  { id: v4(), color: "blue", display: "block", screenCount: 2 },
+  { id: v4(), color: "yellow", display: "block", screenCount: 3 },
+  { id: v4(), color: "orange", display: "block", screenCount: 4 },
+  { id: v4(), color: "green", display: "block", screenCount: 5 },
+];
 
 const Home = () => {
+  const [input, setInput] = useState("");
 
-      const [input,setInput]=useState("")
+  const [color, setColor] = useState(initialData);
 
-      const [color,setColor]=useState([]);
+  const [shootingColor, setShootingColor] = useState([]);
 
-       const [shootingColor,setShootingColor]=useState([])
-
-      let arr=[];
-
-      let randomColor=()=>{
-     
-         let r=Math.floor(Math.random()*255)
-         let g=Math.floor(Math.random()*255)
-         let b=Math.floor(Math.random()*255)
-       arr.push(`rgb(${r},${g},${b})`)
-
+  const moveToBox = (id) => {
+    const newColor = color.map((item) => {
+      if (item.screenCount === +input) {
+        item.display = "none";
+        item.screenCount = 0;
+        setShootingColor((prev) => [...prev, item]);
       }
+      return item;
+    });
+    let count = 1;
+    const newOrder = newColor.map((item) => {
+      if (item.display === "block") {
+        item.screenCount = count;
+        count++;
+      }
+      return item;
+    });
 
-      useMemo(()=>{
-      
-        for(let i=0;i<5;i++){
-           randomColor()
+    setColor(newOrder);
+  };
+
+  useEffect(() => {
+    console.log(shootingColor);
+  }, [shootingColor]);
+
+  const moveToCircle = (id) => {
+    const newShootingColorArray = shootingColor.filter(
+      (item) => item.id !== id
+    );
+    setShootingColor(newShootingColorArray);
+
+    const newColorArray = color.map((item) => {
+      if (item.id === id) {
+        item.display = "block";
+      }
+      return item;
+    });
+
+     let count=1;
+      const newScreenOrder=newColorArray.map((item) => {
+        if (item.display === "block") {
+          item.screenCount = count;
+          count++;
         }
+        return item;
+      });
 
-        setColor(arr)
-      },[])
-
-
-      //  move to box 
-       const moveToBox=()=>{
-
-        setShootingColor([...shootingColor ,...color.splice(input-1,1)])
-
-        
-
-       }
-
-       // move  box to circle div
-        
-        const moveToCircle=(index)=>{
-
-          setColor([...color ,...shootingColor.splice(index,1)])
-          
-
-        }
-
-
-
-
-
-
+       setColor(newScreenOrder)
+  };
 
   return (
     <div className="container">
-        
-        <div className="emptyDiv">
-        {
-          shootingColor && shootingColor.map((colorItem,index)=>{
-
-              return  <div key={index} className="circle boxcircle"
-              
-              style={{ backgroundColor: `${colorItem}` }}
-          
-               onClick={()=>{moveToCircle(index) }}
-
-              >
-              </div>
-
-              })
-
-              }
-          
-
-
-
-        </div>
-
-
-    <div className="circleDiv">
-
-              {
-              color && color.map((colorItem,index)=>{
-
-              return  <div key={index} className="circle"
-              
-              style={{ backgroundColor: `${colorItem}` }}
-              >
-              </div>
-
-              })
-
-              }
-          
-
-    </div>
-      
-      <div className="buttonDiv">
-
-             <input type="number"
-              value={input}
-              placeholder="Enter Number"
-              onChange={(event)=> setInput(event.target.value)}
-
-              />
-             <button
-              
-              onClick={() => moveToBox()}
-             >shoot</button>
-           
-
-
+      <div className="emptyDiv">
+        {shootingColor[0] &&
+          shootingColor.map((colorItem) => {
+            return (
+              <div
+                key={colorItem.id}
+                className="circle boxcircle"
+                style={{ backgroundColor: `${colorItem.color}` }}
+                onClick={() => {
+                  moveToCircle(colorItem.id);
+                }}
+              ></div>
+            );
+          })}
       </div>
-    .
-    
-    
-    
-    
-    
-    
+      <div className="circleDiv">
+        {color &&
+          color.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="circle"
+                style={{
+                  backgroundColor: `${item.color} `,
+                  display: `${item.display}`,
+                }}
+              ></div>
+            );
+          })}
+      </div>
+      <div className="buttonDiv">
+        <input
+          type="number"
+          value={input}
+          placeholder="Enter Number"
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button onClick={() => moveToBox()}>shoot</button>
+      </div>
+      .
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
